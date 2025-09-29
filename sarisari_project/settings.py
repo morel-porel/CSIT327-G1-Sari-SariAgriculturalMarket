e61ae2331f13dccb91a307746d158204a3d4f315
morel-porel <paciomuriel@gmail.com>
2025-09-30 00:18:55 +0800

    feature(users): Implement user registration and login pages

--- sarisari_project/settings.py
+++ sarisari_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        VENDOR = "VENDOR", "Vendor"
+        CONSUMER = "CONSUMER", "Consumer"
+
+    # Add any common fields here, like a phone number
+    phone_number = models.CharField(max_length=15, blank=True, null=True, unique=True)
+    
+    # This is the crucial field for role-based access
+    role = models.CharField(max_length=50, choices=Role.choices)
