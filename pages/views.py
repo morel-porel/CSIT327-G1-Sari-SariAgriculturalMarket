93de889ae908299904dcb4ee71e64cbaffc6ea08
Gerard <gerard081304@gmail.com>
2025-10-18 03:29:50 +0800

    added product lsiting page and database for it

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
+# pages/views.py
+
 from django.shortcuts import render
+from products.models import Product # Import the Product model
 
 def about_us_view(request):
     # This view's only job is to render the 'about.html' template.
     return render(request, 'pages/about.html')
 
 def home_view(request):
-    return render(request, 'pages/home.html')
+    products = Product.objects.all().order_by('-created_at') # Get all products
+    context = {
+        'products': products
+    }
+    return render(request, 'pages/home.html', context)
