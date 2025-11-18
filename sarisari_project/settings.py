b4f30fef13d65fa0fa424480409738bffec6b30c
morel-porel <paciomuriel@gmail.com>
2025-11-19 01:23:52 +0800

    bugfix: image upload

--- sarisari_project/settings.py
+++ sarisari_project/settings.py
@@ -193,6 +193,16 @@ else:
     AWS_S3_CUSTOM_DOMAIN = f"{_supabase_project_id}.supabase.co/storage/v1/object/public/{AWS_STORAGE_BUCKET_NAME}"
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
+# At the end of settings.py
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('SUPABASE_BUCKET')
+AWS_S3_ENDPOINT_URL = f"{os.getenv('SUPABASE_URL')}/storage/v1/s3"
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_FILE_OVERWRITE = False
+
 
 # Email settings for password reset
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
