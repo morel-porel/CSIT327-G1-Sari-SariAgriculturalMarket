ab8203d89786f70e8fa53b8cd490511b5bd748b2
Gerard <gerard081304@gmail.com>
2025-12-02 23:20:50 +0800

    Fix: messages are now in PHP time

--- sarisari_project/settings.py
+++ sarisari_project/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'  # Philippine Standard Time (UTC+8)
 USE_I18N = True
 USE_TZ = True
 
